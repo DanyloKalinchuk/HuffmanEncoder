96cbfbb924090d651e4f920bd02f1c50e538ea6e
DanyloKalinchuk <kalinchukdanylo@gmail.com>
2025-05-26 18:57:26 +0200

    Add files via upload

---
+++ build/CMakeFiles/HuffmanEncoder.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuffmanEncoder.dir/huffmanEncoder.cpp.obj"
+  "CMakeFiles/HuffmanEncoder.dir/huffmanEncoder.cpp.obj.d"
+  "CMakeFiles/HuffmanEncoder.dir/main.cpp.obj"
+  "CMakeFiles/HuffmanEncoder.dir/main.cpp.obj.d"
+  "HuffmanEncoder.exe"
+  "HuffmanEncoder.exe.manifest"
+  "HuffmanEncoder.pdb"
+  "libHuffmanEncoder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HuffmanEncoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ huffmanEncoder.hpp
@@ -0,0 +1,49 @@
+#ifndef HUFFMANENCODER_HPP
+#define HUFFMANENCODER_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string decode_huffman(map<string, char>, string);
+
+class Node{
+    public:
+        const char character;
+        const int frequency;
+        Node* left = nullptr;
+        Node* right = nullptr;
+
+        Node(char character, int frequency, Node* left, Node* right)
+            : character(character), frequency(frequency), left(left), right(right) {}
+        int get_frequency() const;
+        char get_character() const;
+};
+
+class HuffmanTree{
+    public:
+        Node* root = nullptr;
+
+        void nodes_to_tree(vector<Node*>);
+};
+
+class HuffmanEncoder{
+    public:
+        string encode(string);
+        string get_original();
+
+    private:
+        string encoded = "";
+        map<char, string> codes;
+        map<string, char> code_to_char;
+        map<char, int> frequences;
+        HuffmanTree tree;
+
+        void count_frequences(string);
+        vector<Node*> to_nodes();
+        void dfs_encoding(Node*, string);
+};
+
+#endif
